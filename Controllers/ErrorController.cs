4238ec4fcff70bb2e0259e5e5317e51a8b9f2a6c
Mikhail Govender <mikhailg@bbd.co.za>
2023-07-10 11:23:14 +0200

    Add Global Error Handling

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIv1.Controllers
+{
+    /*
+     * Implementation of Global Error Handling
+     */
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        [HttpGet()]
+        public IActionResult Error()
+        {
+            return Problem();
+        }
+    }
+}
